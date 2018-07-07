6e0852c835d7bf0d9b16ec296b9ad131019b1414
duanlei <1969563716@qq.com>
2018-07-07 20:40:02 +0800

    main 居中

--- resume/src/App.vue
+++ resume/src/App.vue
@@ -1,23 +1,69 @@
 <template>
-  <div>
-    <p>{{text}}~</p>
+  <div class="page">
+    <header>
+      <TopBar/>
+    </header>
+    <main>
+      <ResumeEditor/>
+      <ResumePreview/>
+    </main>
+
   </div>
 
 </template>
 
 <script>
-export default {
-  name: 'App',
-  data: function(){
-    return {
-      text: '你好呀~ '
+  import './assets/reset.css'
+  import 'normalize.css/normalize.css'
+  import TopBar from './components/TopBar.vue'
+  import ResumeEditor from './components/ResumeEditor.vue'
+  import ResumePreview from './components/ResumePreview.vue'
+
+
+  export default {
+    name: 'App',
+    components: {TopBar, ResumeEditor, ResumePreview},
+    data: function () {
+      return {
+        text: '你好呀~ '
+      }
     }
   }
-}
 </script>
 
 <style>
-p{
-  color: red
-}
+  .page {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    background: #EAEBEC;
+  }
+
+  .page > main {
+    align-self: center;
+    width: 100%;
+    /*这两句是为了修复当分辨率大于1440时，main不居中的问题*/
+    flex-grow: 1;
+    min-width: 1024px;
+    max-width: 1440px;
+    margin-top: 16px;
+    margin-bottom: 16px;
+    display: flex;
+    justify-content: space-between;
+    padding: 0 16px;
+  }
+
+  #resumeEditor {
+    width: 35%;
+    background: #444;
+
+  }
+
+  #resumePreview {
+    flex-grow: 1;
+    margin-left: 16px;
+    background: #777;
+
+  }
+
 </style>
---
+++ resume/src/components/TopBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="topBar">
+    <div class="wrapper">
+      <span class="logo">
+        resumer
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'TopBar'
+  }
+</script>
+
+<style>
+  #topBar {
+    background: #fff;
+    box-shadow: 0 1px 3px 0 rgba(0, 0, 0, .3);
+    height: 64px;
+  }
+
+  #topBar > .wrapper {
+    min-width: 1024px;
+    max-width: 1440px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 16px;
+  }
+
+  #topBar .logo {
+    font-size: 24px;
+    color: #000;
+  }
+</style>
