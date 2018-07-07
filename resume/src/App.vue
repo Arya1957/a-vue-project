74ebf78363bca1891c62f6cca291b5f1a2d3d1f5
duanlei <1969563716@qq.com>
2018-07-07 16:46:58 +0800

    addfile

---
+++ resume/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>{{text}}~</p>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: function(){
+    return {
+      text: '你好呀~ '
+    }
+  }
+}
+</script>
+
+<style>
+p{
+  color: red
+}
+</style>
