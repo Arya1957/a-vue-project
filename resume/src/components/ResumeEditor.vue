6ff0bc5d70b5f3a19b8acf528887c7e09c66a6d7
duanlei <1969563716@qq.com>
2018-07-08 09:56:14 +0800

    使用脚本将SVG 文件拼成一个

--- resume/src/components/ResumeEditor.vue
+++ resume/src/components/ResumeEditor.vue
@@ -1,18 +1,75 @@
 <template>
   <div id="resumeEditor">
-    resumeEditor
+   <nav>
+     <ol>
+       <li v-for="(item,index) in resume.visibleItems"
+         v-bind:class="{active: item === selected}"
+           v-on:click = "selected = item"
+       >{{index}}</li>
+     </ol>
+   </nav>
+    <ol class="panels">
+      <li v-for="item in resume.visibleItems"
+          v-show = "item === selected">
+        {{resume[item]}}   </li>
+    </ol>
   </div>
 </template>
 
 
 <script>
   export default{
-    name: 'ResumeEditor'
+    name: 'ResumeEditor',
+    data: function (){
+      return {
+        selected: 'bio',
+        resume: {
+          visibleItems: ['bio','workHistory','education','projects','awards','contacts','others'],
+          bio: {
+            name: '王大锤',
+            city: '天天',
+            title: 'shsjsk'
+          },
+          'workHistory': [],
+          'education': [],
+          'projects': [],
+          'awards': [],
+          'contacts': [],
+          'others': []
+        }
+      }
+    }
   }
 </script>
 
-<style>
+<style lang="scss">
   #resumeEditor{
     background: #fff;
-    box-shadow: 0 1px 3px 0 rgba(0,0,0,.3)  }
+    box-shadow: 0 1px 3px 0 rgba(0,0,0,.3) ;
+    display: flex;
+    flex-direction: row;
+    >nav {
+      width: 80px;
+      background: black;
+      color: white;
+      >ol {
+        >li {
+          height: 48px;
+          display: flex;
+          justify-content: space-around;
+          align-items: center;
+          margin-top: 16px;
+          margin-bottom: 16px;
+          &.active{
+            background: #fff;
+            color: black;
+          }
+        }
+      }
+    }
+  }
+
+  ol {
+    list-style: none;
+  }
 </style>
--- resume/src/components/TopBar.vue
+++ resume/src/components/TopBar.vue
@@ -4,6 +4,10 @@
       <span class="logo">
         resumer
       </span>
+      <div class="actions">
+        <button class="primary">保存</button>
+        <button>预览</button>
+      </div>
     </div>
   </div>
 </template>
@@ -14,25 +18,43 @@
   }
 </script>
 
-<style>
+<style lang="scss" scoped>
   #topBar {
     background: #fff;
     box-shadow: 0 1px 3px 0 rgba(0, 0, 0, .3);
     height: 64px;
+
+    > .wrapper {
+      min-width: 1024px;
+      max-width: 1440px;
+      margin: 0 auto;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 0 16px;
+    }
+
+    .logo {
+      font-size: 24px;
+      color: #000;
+    }
   }
 
-  #topBar > .wrapper {
-    min-width: 1024px;
-    max-width: 1440px;
-    margin: 0 auto;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0 16px;
+  button {
+    width: 72px;
+    height: 32px;
+    border: none;
+    cursor: pointer;
+    font-size: 18px;
+    background: #ddd;
+    color: #222;
+    &:hover {
+      box-shadow: 1px 1px 1px hsla(0,0,0,0.5)
+    }
+    &.primary {
+      background: #02af5f;
+      color: #fff;
+    }
   }
 
-  #topBar .logo {
-    font-size: 24px;
-    color: #000;
-  }
 </style>
